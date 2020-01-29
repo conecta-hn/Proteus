1ee0138b83ad79356a9e1f8d84026636456ddfc7
César Morgan <xds_xps_ivx@hotmail.com>
2020-01-29 11:11:21 -0600

    WIP: Controles de búsqueda mejorados

--- src/Clients/ProteusWorkstation/ViewModels/ListEditorViewModel.cs
+++ src/Clients/ProteusWorkstation/ViewModels/ListEditorViewModel.cs
@@ -23,6 +23,8 @@ using TheXDS.Proteus.Misc;
 using System.Data.Entity;
 using TheXDS.Proteus.Crud;
 using TheXDS.Proteus.Config;
+using TheXDS.MCART;
+using TheXDS.Proteus.Component;
 
 namespace TheXDS.Proteus.ViewModels
 {
@@ -438,7 +440,12 @@ namespace TheXDS.Proteus.ViewModels
         private async Task PerformSearch()
         {
             IsSearching = true;
-            Results = CollectionViewSource.GetDefaultView(await Internal.Query(SearchQuery!, ActiveModel!).ToListAsync());
+            var l = await Internal.Query(SearchQuery!, ActiveModel!).Cast<ModelBase>().ToListAsync();
+            foreach(var j in Objects.FindAllObjects<IModelLocalSearchFilter>())
+            {
+                l = j.Filter(l, SearchQuery!);
+            }
+            Results = CollectionViewSource.GetDefaultView(l);
             IsSearching = false;
             WillSearch = false;
         }
