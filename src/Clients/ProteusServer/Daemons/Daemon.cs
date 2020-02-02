6a1dc1c51710d0abdc9d8be2afc43c3a3f1ab4a8
César Andrés Morgan <xds_xps_ivx@hotmail.com>
2020-02-02 12:32:15 -0600

    Fixes menores

--- src/Clients/ProteusServer/Daemons/Daemon.cs
+++ src/Clients/ProteusServer/Daemons/Daemon.cs
@@ -8,8 +8,16 @@ using TheXDS.MCART.PluginSupport.Legacy;
 
 namespace TheXDS.Proteus.Daemons
 {
+    /// <summary>
+    /// Clase base para un objeto que describa acciones que pueden ejecutarse
+    /// desde el servidor de forma periódica.
+    /// </summary>
     public abstract class Daemon : Plugin, IDaemon
     {
+        /// <summary>
+        /// Obtiene un valor que indica si este <see cref="Daemon"/> puede ser
+        /// ejecutado.
+        /// </summary>
         public virtual bool CanRun => true;
 
         /// <summary>
@@ -17,8 +25,14 @@ namespace TheXDS.Proteus.Daemons
         /// </summary>
         public virtual byte Schedule => (byte)(DateTime.Now.TimeOfDay.TotalMinutes / 15);
 
+        /// <summary>
+        /// Obtiene una programación de tiempo en la cual ejecutar el Daemon.
+        /// </summary>
         public TimeSpan ScheduleTime => TimeSpan.FromMinutes(Schedule * 15);
 
+        /// <summary>
+        /// Ejecuta el <see cref="Daemon"/>.
+        /// </summary>
         public abstract void Run();
     }
 }
