6c59cc8873996c8e7c9af0aaf658b2f8b67d0077
César Morgan <xds_xps_ivx@hotmail.com>
2020-02-03 16:20:09 -0600

    fix de constructor incorrecto

--- src/UiModules/ConectaModule/ViewModels/InversorViewModel.cs
+++ src/UiModules/ConectaModule/ViewModels/InversorViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using TheXDS.MCART.ViewModel;
 using TheXDS.Proteus.Conecta.Models;
+using TheXDS.Proteus.Crud;
 
 namespace TheXDS.Proteus.Conecta.ViewModels
 {
@@ -8,13 +9,13 @@ namespace TheXDS.Proteus.Conecta.ViewModels
     /// dentro del Crud generado para el modelo
     /// <see cref="Inversor"/>.
     /// </summary>
-    public class InversorViewModel : ViewModel<Inversor>
+    public class InversorViewModel : ProteusEntityViewModel<Inversor>
     {
         /// <summary>
         /// Inicializa una nueva instancia de la clase
         /// <see cref="InversorViewModel"/>.
         /// </summary>
-        protected InversorViewModel()
+        public InversorViewModel()
         {
             RegisterPropertyChangeBroadcast(nameof(Entity.Inversion), nameof(Entity.InvTotal));
         }
