ede6d42fb9ebd37a4879f2a74ff1d0df44dbef75
César Andrés Morgan <xds_xps_ivx@hotmail.com>
2020-02-02 23:15:54 -0600

    Fixes de docs

--- src/Proteus/Api/Result.cs
+++ src/Proteus/Api/Result.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*
+Copyright © 2017-2020 César Andrés Morgan
+Licenciado para uso interno solamente.
+*/
+
+using System;
 using TheXDS.MCART.Attributes;
 
 namespace TheXDS.Proteus.Api
@@ -33,6 +38,10 @@ namespace TheXDS.Proteus.Api
         [Name("No ha sido posible contactar al servidor de datos.")]
         Unreachable = 4
     }
+
+    /// <summary>
+    /// Describe un resultado de operación detallado.
+    /// </summary>
     public class DetailedResult
     {
         /// <summary>
@@ -56,44 +65,98 @@ namespace TheXDS.Proteus.Api
         /// </summary>
         public static readonly DetailedResult Unreachable = new DetailedResult(Result.Unreachable);
 
+        /// <summary>
+        /// Valor de resultado de la operación.
+        /// </summary>
         public Result Result { get; }
+
+        /// <summary>
+        /// Mensaje descriptivo sobre el resultado de la operación.
+        /// </summary>
         public string Message { get; }
+
+        /// <summary>
+        /// Inicializa una nueva instancia de la clase
+        /// <see cref="DetailedResult"/>.
+        /// </summary>
         public DetailedResult() : this(Result.Ok)
         {
         }
+
+        /// <summary>
+        /// Inicializa una nueva instancia de la clase
+        /// <see cref="DetailedResult"/> cuando la misma ha fallado.
+        /// </summary>
+        /// <param name="message">
+        /// Mensaje que describe el motivo por el cual la operación falló.
+        /// </param>
         public DetailedResult(string message)
         {
             Result = Result.Fail;
             Message = message;
         }
+
+        /// <summary>
+        /// Inicializa una nueva instancia de la clase
+        /// <see cref="DetailedResult"/>.
+        /// </summary>
+        /// <param name="result">Resultado de la operación.</param>
+        /// <param name="message">
+        /// Mensaje descriptivo sobre el resultado de la operación.
+        /// </param>
         public DetailedResult(Result result, string message)
         {
             Result = result;
             Message = message;
         }
+
+        /// <summary>
+        /// Inicializa una nueva instancia de la clase
+        /// <see cref="DetailedResult"/>.
+        /// </summary>
+        /// <param name="result">Resultado de la operación.</param>
         public DetailedResult(Result result)
         {
             Result = result;
-            switch (result)
+            Message = result switch
             {
-                case Result.Ok:
-                    Message = "Operación completada correctamente.";
-                    break;
-                case Result.Fail:
-                    Message = "Se ha producido un error realizando la operación.";
-                    break;
-                case Result.Forbidden:
-                    Message = "Acceso denegado.";
-                    break;
-                case Result.Unreachable:
-                    Message = "No ha sido posible contactar al servidor.";
-                    break;
-            }
-
+                Result.Ok => "Operación completada correctamente.",
+                Result.Fail => "Se ha producido un error realizando la operación.",
+                Result.Forbidden => "Acceso denegado.",
+                Result.Unreachable => "No ha sido posible contactar al servidor.",
+                _ => "La operación ha finalizado debido a un problema desconocido.",
+            };
         }
+
+        /// <summary>
+        /// Convierte implícitamente un <see cref="string"/> en un
+        /// <see cref="DetailedResult"/>.
+        /// </summary>
+        /// <param name="message"><see cref="string"/> a convertir.</param>
         public static implicit operator DetailedResult(string message) => new DetailedResult(message);
+
+
+        /// <summary>
+        /// Convierte implícitamente un <see cref="DetailedResult"/> en un
+        /// <see cref="string"/>.
+        /// </summary>
+        /// <param name="result"><see cref="DetailedResult"/> a convertir.</param>
         public static implicit operator string(DetailedResult result) => result.Message;
+
+
+        /// <summary>
+        /// Convierte implícitamente un <see cref="Result"/> en un
+        /// <see cref="DetailedResult"/>.
+        /// </summary>
+        /// <param name="result"><see cref="Result"/> a convertir.</param>
         public static implicit operator DetailedResult(Result result) => new DetailedResult(result);
+
+
+        /// <summary>
+        /// Convierte implícitamente un <see cref="DetailedResult"/> en un
+        /// <see cref="Result"/>.
+        /// </summary>
+        /// <param name="result"><see cref="DetailedResult"/> a convertir.</param>
         public static implicit operator Result(DetailedResult result) => result.Result;
     }
 }
--- src/Proteus/Seeders/WebServiceUserSeeder.cs
+++ src/Proteus/Seeders/WebServiceUserSeeder.cs
@@ -1,5 +1,5 @@
 ﻿/*
-Copyright © 2017-2019 César Andrés Morgan
+Copyright © 2017-2020 César Andrés Morgan
 Licenciado para uso interno solamente.
 */
 
@@ -10,19 +10,51 @@ using TheXDS.Proteus.Models;
 
 namespace TheXDS.Proteus.Seeders
 {
+    /// <summary>
+    /// Semillador que genera un token de seguridad predeterminado para el
+    /// usuario de API Web.
+    /// </summary>
     [SeederFor(typeof(UserService))]
     public class WebServiceUserSeeder : IAsyncDbSeeder
     {
-        public async Task<DetailedResult> SeedAsync(IFullService service, IStatusReporter reporter)
+        /// <summary>
+        /// Ejecuta la acción de semillado de la base de datos de forma
+        /// asíncrona.
+        /// </summary>
+        /// <param name="service">
+        /// Servicio sobre el cual ejecutar las acciones de semillado.
+        /// </param>
+        /// <param name="reporter">
+        /// Objeto que permite reportar el progreso de la operación.
+        /// </param>
+        /// <returns>
+        /// El resultado detallado de la operación de semillado de la base de
+        /// datos.
+        /// </returns>
+        public async Task<DetailedResult> SeedAsync(IFullService service, IStatusReporter? reporter)
         {
             reporter?.UpdateStatus("Creando tokens para usuarios de servicio...");
-            var us = service as UserService;
+            var us = (UserService)service;
             var r = await us.GenerateToken("apiwebservice", null, null, out var token);
             reporter?.UpdateStatus(100,$"Token para 'apiwebservice':\n{token}");
             return r;
         }
 
-        public async Task<bool> ShouldRunAsync(IReadAsyncService service, IStatusReporter reporter)
+        /// <summary>
+        /// Realiza comprobaciones sobre la base de datos para determinar si
+        /// debe ejecutarse o no este semillador.
+        /// </summary>
+        /// <param name="service">
+        /// Servicio sobre el cual ejecutar las acciones de semillado.
+        /// </param>
+        /// <param name="reporter">
+        /// Objeto que permite reportar el progreso de la operación.
+        /// </param>
+        /// <returns>
+        /// <see langword="true"/> si debe ejecutarse el semillado en la base
+        /// de datos, <see langword="false"/> en caso contrario.
+        /// </returns>
+        public async Task<bool> ShouldRunAsync(IReadAsyncService service, IStatusReporter? reporter)
         {
             reporter?.UpdateStatus("Comprobando tokens de servicios...");
             return !await service.AnyAsync<LoginToken>(p => p.Login.Id == "apiwebservice");
