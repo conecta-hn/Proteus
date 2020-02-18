726c6622ce27fdfa878acce954ecce1a32282891
César Morgan <xds_xps_ivx@hotmail.com>
2020-02-18 11:00:01 -0600

    WIP: script de actualización

--- targets/update-push.ps1
+++ targets/update-push.ps1
@@ -1,5 +1,6 @@
 param(
-    [System.String]$branch
+    [System.String]$branch,
+    [System.String]$publishPath = "D:\ProteusData"
 )
 
 function Get-ScriptDirectory
@@ -11,7 +12,18 @@ function Get-ScriptDirectory
 $buildPath = [System.IO.path]::GetFullPath("$(Get-ScriptDirectory)\..\Build\bin\$project\Debug\netcoreapp3.1")
 $targetPath = [System.IO.path]::GetFullPath("$(Get-ScriptDirectory)\..\Build\bin\$target\Debug\netcoreapp3.1")
 
+$roth = [System.IO.path]::GetFullPath("$(Get-ScriptDirectory)/..").ToString()
+Set-Location $roth
+
 if (-not [System.String]::IsNullOrWhiteSpace($project))
 {
-    Invoke-Command "git chechout"
+    Invoke-Expression "git chechout $branch"
 }
+
+Invoke-Expression "git fetch"
+Invoke-Expression "git pull"
+Invoke-Expression "dotnet build $roth\src\Proteus.sln"
+Copy-Item $([System.IO.path]::Combine($roth,"Build\bin\UpdatePusher\Debug\netcoreapp3.1\*")) $([System.IO.path]::Combine($roth,"Build\bin\ProteusWorkstation\Debug\netcoreapp3.1"))
+Invoke-Expression "$([System.IO.path]::Combine($roth,"Build\bin\ProteusWorkstation\Debug\netcoreapp3.1\UpdatePusher.exe"))"
+Copy-Item $([System.IO.path]::Combine($roth,"Build\bin\ProteusWorkstation\Debug\netcoreapp3.1\*")) $([System.IO.path]::Combine($publishPath,"release"))
+Move-Item $([System.IO.path]::Combine($publishPath,"release\release.manifest")) $publishPath
