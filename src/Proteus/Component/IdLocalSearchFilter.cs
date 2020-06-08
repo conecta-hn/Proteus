5da467c133b3c2ac4dcdad863bf4750a44bc72c6
César Andrés Morgan <xds_xps_ivx@hotmail.com>
2020-06-08 17:07:55 -0600

    Expansión menor de API para filtros de búsqueda

---
+++ src/Proteus/Component/IdLocalSearchFilter.cs
@@ -0,0 +1,22 @@
+﻿/*
+Copyright © 2017-2020 César Andrés Morgan
+Licenciado para uso interno solamente.
+*/
+
+using System.Collections.Generic;
+using System;
+using TheXDS.Proteus.Models.Base;
+using System.Linq;
+
+namespace TheXDS.Proteus.Component
+{
+    public class IdLocalSearchFilter : IModelLocalSearchFilter
+    {
+        public List<TModel> Filter<TModel>(List<TModel> collection, string query) where TModel : ModelBase
+        {
+            return collection.Where(p => p.StringId.Contains(query)).ToList();
+        }
+
+        public bool UsableFor(Type model) => true;
+    }
+}
