59af0860bffbbe5968ca03c20ed134af2397aedb
César Andrés Morgan <xds_xps_ivx@hotmail.com>
2020-09-10 22:11:12 -0600

    CrudTools para códigos de barra

---
+++ src/UiModules/FacturacionUi/Lib/BarcodeHelper.cs
@@ -0,0 +1,48 @@
+﻿/*
+Copyright © 2017-2020 César Andrés Morgan
+Licenciado para uso interno solamente.
+*/
+
+using BarcodeLib;
+using System.Drawing;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media;
+using TheXDS.MCART;
+using WpfScreenHelper;
+
+namespace TheXDS.Proteus.FacturacionUi.Lib
+{
+    internal static class BarcodeHelper
+    {
+        public static Visual RenderBarcode(TYPE barcodeType, string id)
+        {
+            var v = new DrawingVisual();
+            using var dc = v.RenderOpen();
+            using var barcode = new Barcode
+            {
+                IncludeLabel = true,
+                LabelPosition = LabelPositions.BOTTOMCENTER,
+                ImageFormat = System.Drawing.Imaging.ImageFormat.Png,
+                LabelFont = new Font("Consolas", 24)
+            };
+            var img = barcode.Encode(barcodeType, id);
+            dc.DrawImage(img.ToSource(), new Rect { Width = img.Width, Height = img.Height });
+            return v;
+        }
+    }
+
+    internal static class ScreenHelper
+    {
+        public static void ToScreen(this Window window, byte screen)
+        {
+            var sc = Screen.AllScreens.ToList();
+            var s = (screen >= sc.Count ? sc.Last() : sc[screen-1]).Bounds;
+            window.Top = s.Top;
+            window.Left = s.Left;
+            window.Width = s.Width;
+            window.Height = s.Height;
+            window.WindowState = WindowState.Maximized;
+        }
+    }
+}
---
+++ src/UiModules/FacturacionUi/Tools/FacturableBarcodeCrudTool.cs
@@ -0,0 +1,56 @@
+﻿/*
+Copyright © 2017-2020 César Andrés Morgan
+Licenciado para uso interno solamente.
+*/
+
+using BarcodeLib;
+using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using TheXDS.MCART.Types.Extensions;
+using TheXDS.Proteus.Crud;
+using TheXDS.Proteus.Dialogs;
+using TheXDS.Proteus.Models.Base;
+using TheXDS.Proteus.Plugins;
+using TheXDS.Proteus.ViewModels.Base;
+using TheXDS.Proteus.Widgets;
+using static TheXDS.Proteus.FacturacionUi.Lib.BarcodeHelper;
+
+namespace TheXDS.Proteus.FacturacionUi.Tools
+{
+    public class FacturableBarcodeCrudTool : CrudTool<Facturable>
+    {
+        public FacturableBarcodeCrudTool() : base(CrudToolVisibility.Selected)
+        {
+        }
+
+        public override IEnumerable<Launcher> GetLaunchers(IEnumerable<Type> models, ICrudViewModel? vm)
+        {
+            foreach (var j in models)
+            {
+                yield return Launcher.FromMethod(
+                    "Generar código de barras",
+                    $"Permite generar un código de barras para este tipo de {CrudElement.GetDescription(j)?.FriendlyName ?? j.Name}.",
+                    GenBarcode, () => vm);
+            }
+        }
+
+        private void GenBarcode(ICrudViewModel? vm)
+        {
+            var m = TYPE.CODE128B;
+            if (!InputSplash.Get("Seleccione el tipo de código de barra a generar", ref m)) return;
+            try
+            {
+                var pd = new PrintDialog();
+                if (pd.ShowDialog() ?? false)
+                {
+                    pd.PrintVisual(RenderBarcode(m, vm!.Selection!.StringId), $"Código de barras {m.NameOf()} para {vm!.SelectedElement!.Description.FriendlyName} {vm!.Selection!.StringId} - {App.Info.Name}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Proteus.MessageTarget?.Warning($"No se pudo utilizar el tipo {m.NameOf()} para generar el código de barra: {ex.Message}");
+            }
+        }
+    }
+}
